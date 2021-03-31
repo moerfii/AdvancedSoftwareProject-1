ffc59dfa20a5adb0ead13bf1dcae493545a56393
flruee <florian.rueegsegger@gmail.com>
2021-03-31 21:14:17 +0200

    testing with api

--- Frontend/src/main/python/main.py
+++ Frontend/src/main/python/main.py
@@ -1,12 +1,25 @@
 from kivy.app import App
 from kivy_garden.mapview import MapMarkerPopup
-
+import requests
+import json
+import time
+import copy
 
 class MainApp(App):
     def on_start(self):
-        marker = MapMarkerPopup(lat=40.754, lon=-73.984)
-        self.root.add_widget(marker)
-    pass
+        lat1, lon1, lat2, lon2=self.root.get_bbox()
+        data = requests.get(f"http://localhost:8888/listings/location?lat1={lat1}&lat2={lat2}&lon1={lon1}&lon2={lon2}")
+        data = json.loads(data.text)
+        start = time.time()
+        print(len(data))
+        for i in range(len(data)):
+            print(i)
+            marker=MapMarkerPopup(lat=data[i]["latitude"],lon=data[i]["longitude"], source="marker.png")
+
+            self.root.add_marker(marker)
+
+        print(time.time()-start)
+        
 
 
 MainApp().run()
