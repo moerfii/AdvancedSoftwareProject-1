49d0c5c2219427cd26cac81b549561ccc1f87e80
moerfii <55541119+moerfii@users.noreply.github.com>
2021-04-10 17:24:55 +0200

    unsure

--- Frontend/src/main/python/listingDetail.py
+++ Frontend/src/main/python/listingDetail.py
@@ -3,20 +3,20 @@ from kivy.uix.label import Label
 import requests
 import json
 
-
 def listingDetail(listing_id):
     response = requests.get(f"http://localhost:8888/listing/{listing_id}")
     data = json.loads(response.text)[0]
     print(data)
     popup = Popup(
         title=data['name'],
-        content=Label(text=f"""
+        content=Label(text = f"""
             Price: {data['price']}$/night
             RoomType: {data['room_type']}
             """
-                      ),
-        size_hint=(None, None),
-        size=(400, 400),
-        auto_dismiss=True)
-
+        ),
+        size_hint=(None,None),
+        size=(400,400),
+        auto_dismiss=False)
+    
     return popup
+    
--- rest_api/src/server.js
+++ rest_api/src/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 const express = require("express");
 const { Pool } = require("pg");
 const cors = require("cors");
-const { query } = require("express");
+const axios = require("axios")
 
 PORT = 8888
 
@@ -21,7 +21,41 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD  
 })
 
+const operators = {
+    "eq":"=",
+    "ge":">=",
+    "le":"<="
+}
 
+
+function buildQuery(queryString,query,setAND=false) {
+    /*  Adds WHERE clauses to input queryString, cleans query params
+        Inputs: - queryString: A string with a query
+                - query: reqest query dict
+                - count: Set to > 0 if there is already a WHERE constraint 
+    */
+    
+    Object.entries(query).forEach(([key, value]) => {
+        if(setAND) queryString += " AND ";
+        else {
+            queryString += " WHERE ";
+            setAND=true;
+        }
+        key = key.split(".");
+        if(key.length!=2) {
+            return;
+        }
+
+        if(!(key[1] in operators)) {
+            return;
+        }
+        const colName = key[0].replace(/[";]/g,"")
+        const operator = operators[key[1]];
+        const val = value.replace(/[";]/g,"")
+        queryString += `${colName} ${operator} ${val}`
+     });
+     return queryString
+}
 /*
 overview site
 */
@@ -36,14 +70,14 @@ Returns all listings
 app.get(
     "/listings",
     (req,res) => {
+        var query = buildQuery("SELECT * FROM listing",req.query) 
         pool.query(
-            "SELECT * FROM listing;",
+            query,
             (error,result) => {
                 if(error) {
                     console.log(error)
                     res.status(500).send();
                 } else {
-                    console.log(result.rows)
                     res.status(200).send(result.rows);
                 }
             }
@@ -55,18 +89,179 @@ app.get(
 returns locations of all listings
 */
 app.get(
-    "/listings/location",
+    "/listing_location",
     (req,res) => {
-        var lat1 = req.query.lat1 ? Number(req.query.lat1) : -180
-        var lat2 = req.query.lat2 ? Number(req.query.lat2) :  180
-        var lon1 = req.query.lon1 ? Number(req.query.lon1) : -180
-        var lon2 = req.query.lon2 ? Number(req.query.lon2) :  180
+        var query = "SELECT * FROM listing_location"
+        query = buildQuery(query,req.query)
         pool.query(
-            `SELECT id, longitude, latitude
-             FROM listing
-             WHERE longitude >= $1 AND longitude <= $2
-             AND latitude >= $3 AND latitude <= $4;`,
-             [lon1, lon2, lat1, lat2],
+            query,
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+app.get(
+    "/listing_location/:id",
+    (req,res) => {
+        var query = "SELECT * FROM listing_location WHERE id=$1"
+        pool.query(
+            query,
+            [req.params.id],
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+app.get(
+    "/listing_detail",
+    (req,res) => {
+        var query = "SELECT * FROM listing_detail"
+        query = buildQuery(query,req.query)
+        pool.query(
+            query,
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+app.get(
+    "/listing_detail/:id",
+    (req,res) => {
+        var query = "SELECT * FROM listing_detail WHERE id=$1"
+        query = buildQuery(query,req.query)
+        pool.query(
+            query,
+            [req.params.id],
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+
+app.get(
+    "/listing_other",
+    (req,res) => {
+        var query = "SELECT * FROM listing_other"
+        query = buildQuery(query,req.query)
+        pool.query(
+            query,
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+
+app.get(
+    "/listing_other/:id",
+    (req,res) => {
+        var query = "SELECT * FROM listing_other WHERE id=$1"
+        pool.query(
+            query,
+            [req.params.id],
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+app.get(
+    "/listing_reviews",
+    (req,res) => {
+        var query = "SELECT * FROM listing_reviews"
+        query = buildQuery(query,req.query)
+        pool.query(
+            query,
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+app.get(
+    "/listing_reviews/:id",
+    (req,res) => {
+        var query = "SELECT * FROM listing_reviews WHERE id=$1"
+        query = buildQuery(query,req.query)
+        pool.query(
+            query,
+            [req.params.id],
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+app.get(
+    "/host",
+    (req,res) => {
+        var query = "SELECT * FROM host"
+        query = buildQuery(query,req.query)
+        pool.query(
+            query,
+            (error, result) => {
+                if(error) {
+                    console.log(error);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
+app.get(
+    "/host/:id",
+    (req,res) => {
+        var query = "SELECT * FROM host WHERE host_id=$1"
+        query = buildQuery(query,req.query)
+        pool.query(
+            query,
+            [req.params.id],
             (error, result) => {
                 if(error) {
                     console.log(error);
@@ -103,11 +298,12 @@ app.get(
 returns reviews for listing id
 */
 app.get(
-    "/listing/:id/reviews",
+    "/reviews",
     (req,res) => {
+        var query = buildQuery(`SELECT * FROM review`,req.query)
+        
         pool.query(
-            "SELECT * FROM review WHERE listing_id=$1",
-            [req.params.id],
+            query,
             (error, result) => {
                 if(error) {
                     console.log(error)
@@ -120,7 +316,25 @@ app.get(
     }
 )
 
-
+app.get(
+    "/reviews/:id",
+    (req,res) => {
+        var query = `SELECT * FROM review WHERE id=$1`
+        
+        pool.query(
+            query,
+            [req.params.id],
+            (error, result) => {
+                if(error) {
+                    console.log(error)
+                    res.status(500).send()
+                } else {
+                    res.status(200).send(result.rows);
+                }
+            }
+        )
+    }
+)
 /*
 returns specific review for specific listing
 */
@@ -137,6 +351,20 @@ app.get(
                 } else {
                     res.status(200).send(result.rows);
                 }
+            }
+        )
+    }
+)
+
+app.get(
+    "/search_address",
+    async (req,res) => {
+        var key="zleTvt8GJJlclq1BzheL9nXg05bpodk6"
+        await axios({
+            method:"GET",
+            url:`http://mapquestapi.com/geocoding/v1/address?key=zleTvt8GJJlclq1BzheL9nXg05bpodk6&location=${req.query['location']}`,
+        }).then((result) => {
+                res.status(200).send(result.data['results'][0]['locations'][0]['latLng'])
             }
         )
     }
