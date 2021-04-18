9ad5f5fbb5a9ab13994206b7da5faa8364dcc156
flruee <florian.rueegsegger@gmail.com>
2021-04-18 17:29:37 +0200

    changed requirements.txt

--- Frontend/src/main/python/components/airbnbMarker.py
+++ Frontend/src/main/python/components/airbnbMarker.py
@@ -3,11 +3,6 @@ from kivy.properties import NumericProperty
 from listingDetail import listingDetail
 
 class AirbnbMarker(MapMarkerPopup):
-<<<<<<< HEAD:Frontend/src/main/python/components/airbnbMarker.py
-=======
-
-    #id_listing=NumericProperty(0)
->>>>>>> c11c8117beea6e6d85bf4ee2f89ad0f27508349c:Frontend/src/main/python/airbnbmarker.py
     def __init__(self, id_listing, lon, lat, source):
         super(MapMarkerPopup, self).__init__(lon=lon, lat=lat, source=source)
         self.id_listing = id_listing
