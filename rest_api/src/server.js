297a782bf047901a0f96604424c8804d092ed78c
flruee <florian.rueegsegger@gmail.com>
2021-05-29 21:50:23 +0200

    changed ENV variables in docker-compose

--- rest_api/src/server.js
+++ rest_api/src/server.js
@@ -21,7 +21,11 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD  
 })
 
-
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_PORT)
+console.log(process.env.DB_NAME)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)  
 const operators = {
     "eq":"=",
     "ge":">=",
